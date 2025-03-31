8d422871eb5ddd72fc8f29290bbccf62aee40de3
elixing <13976041132@163.com>
2025-04-01 00:21:33 +0800

    fix

--- Scripts/Crontab/ai_club_season_points_incr.php
+++ Scripts/Crontab/ai_club_season_points_incr.php
@@ -5,7 +5,6 @@ namespace FF\Scripts\Crontab;
 use FF\Factory\Bll;
 use FF\Factory\Model;
 use FF\Framework\Utils\Config;
-use FF\Framework\Utils\Log;
 
 if (date('i') != '30' && date('i') != '00') {
     return;
@@ -20,33 +19,8 @@ $seasonInfo = Config::get('club/season', $id, false);
 if (!$seasonInfo) {
     return;
 }
-$gradeCfg = Config::get('club/grade');
-$maxDan = $id - 1;
-$clubIds = [];
-foreach ($gradeCfg as $grade) {
-    if ($grade['id'] > $maxDan) {
-        continue;
-    }
-    $key = Bll::rank()->getClubType($grade['id']);
-    $rankList = Bll::rank()->getList($key, 0, -1);
-    foreach ($rankList as $clubId => $rank) {
-        if (!Bll::club()->isAiClub($clubId)) {
-            continue;
-        }
-        $clubIds[] = $clubId;
-    }
-}
 
-if (!$clubIds) {
-    $clubIds = initSeasonClub($maxDan);
-}
-
-if (!$clubIds) {
-    Log::error("init season club points error", 'club_season.log');
-    return;
-}
-
-$clubList = Model::clubs()->fetchAll(['clubId' => ['in', $clubIds], 'ai' => 1], 'clubId, dan, aiActLevel');
+$clubList = Model::clubs()->fetchAll(['dan' => ['<=', $id - 1], 'ai' => 1], 'clubId, dan, aiActLevel');
 
 $danClubs = [];
 foreach ($clubList as $club) {
@@ -80,39 +54,3 @@ foreach ($danClubs as $_dan => $levelClubs) {
         }
     }
 }
-
-function initSeasonClub($maxDan)
-{
-    $clubIds = [];
-    $danClubs = [];
-    //根据赛季俱乐部信息
-    $clubList = Model::clubs()->fetchAll(['ai' => 1, 'dan' => ['<=', $maxDan]], 'clubId, dan, aiActLevel');
-    foreach ($clubList as $club) {
-        $danClubs[$club['dan']][$club['aiActLevel']][] = $club['clubId'];
-    }
-    $gradeCfg = Config::get('club/grade');
-
-    foreach ($gradeCfg as $grade) {
-        if (!isset($danClubs[$grade['id']])) {
-            continue;
-        }
-        foreach ($grade['aiProportion'] as $inx => $rate) {
-            if ($rate == 0) {
-                continue;
-            }
-            $actLevel = $inx + 1;
-            if (!isset($danClubs[$grade['id']][$actLevel])) {
-                continue;
-            }
-
-            $clubs = $danClubs[$grade['id']][$actLevel];
-            shuffle($clubs);
-            $cnt = min(count($clubs), ceil(count($clubs) * $rate / 10));
-            $clubIds = array_merge($clubIds, array_slice($clubs, 0, $cnt));
-        }
-    }
-
-    return $clubIds;
-}
-
-
--- Scripts/Crontab/init_ai_club_data.php
+++ Scripts/Crontab/init_ai_club_data.php
@@ -1,73 +1,62 @@
 <?php
-
 namespace FF\Scripts\Crontab;
 
 use FF\Bll\ClubBll;
-use FF\Factory\Bll;
 use FF\Factory\Model;
 use FF\Framework\Utils\Config;
 
-//获取参数
-$init = $argv[1] ?? false;
+include __DIR__ . '/../common.php';
 
-if (date('H') != '00' && !$init) {
+$club = Model::clubs()->fetchOne(['dan' => ['<=', 10], 'ai' => 1], 'clubId');
+if ($club) {
     return;
 }
 
-$id = Bll::clubOption()->getSeasonId();
+$clubNames = 'Stormbreaker|Celestial|Phoenix|Moonlight|Thunder|Starry|Crystal|Shadow|Blaze|Aurora|Iron|Eternal|Serpent|Frostborn|Astral|Titan|Reborn|Soulforge|Gaia|Vanguard|allure love|whisper|entai|kite|woundaged|Surehurt|cloudy|Life|Love|my lover|depths|Invader|Daer|mss|sweet|Summer|Nightmare|Cowardice|cardiac|entai|Weirdo|Crayon|catch me|triste|finlandia|pain|tourist|struggle|Misay|delusion|paranoid|S haron|unfinished|soleil|Treason|Doershow|moent|lantau|LAY|feeling|BIGWIN|MEGAWIN|SUPERWIN|JACKPOT|888|struggle|Triste|Aimee|Ending|vapaus|silence|BOOM|Endless|Melted|Proven|SEND OUT|slaughterer|MONSTER|cardiac|Replace|tenderness|Cowardice|whisper|SUnBoC|Beak Hyun|Soul mates|timber|Honoria|destructi|Luminary|ice|BUBU|Rainy|golden|PANDA|JONNY|GodOfWealth|bison|casino|SlotsClub|Perish|kalsotra|divining|ScentFlavor|Uprman|Shadow|D.O|Levy|Triste|Nsane|Shouting|Tourist|AvecleSoleil|FORGOTTEN|STRUGGLE|Forever|Bigbang|HeartSlave|RAINY|ZZang|Fundus|Peach|biubiubiu|Happy|Silence|MoonLight|Jerry|NICEWIN|Baby|OldBaby|Weirdo|Dream|DreamSlots|Stubborn|Juice|ThePupl|Gehenna|Blank|Wind|Eason|Lovely|Cloudy|GiveUp|Melted|Demon|PLAY YOU|Run|Tunder|KissOrHugs|STAY|Endless|CasinoCity|BeLost|TEAM|Hippie|HippieTeam|BOSS|Referee|Judge|SCHOOL|Shadow|PHOENIX|Eclipse|Vortex|Aegis|Ravage|Nova|Onyx|Fury|Ironclad|Storm|Viper|Rogue|Havoc|Titan|Blitz|Wraith|Serpent|Legion|Vanguard|Doom|Rift|Inferno|Celestial|Obsidian|Revenant|Valkyrie|Savage|Thunder|Nemesis|Spectre|Abyss|Risen|Warden|Crimson|Frost|Solaris|Vex|Zenith|Rust|Apex|Void|Chrono|Dread|Mythic|Pandora|Quake|Spartan|Titanic|Umbra|Wolfpack|Xenon|Yggdrasil|Zephyr|Rune|Omen|Lunar|Knight|Juggernaut|Hydra|Glory|Frostbite|Ember|Dragoon|Cursed|Blight|Ashen|Arcanum|Berserk|Chaos|Dragon|Eternal|Fallen|Ghost|Havok|Immortal|Judgment|Kraken|Lich|Maelstrom|Nightfall|Oblivion|Paragon|Quantum|Ragnarok|Stalker|Tempest|Undying|Vendetta|Warlock|Xero|Yojimbo|Zealot|Alpha|Beta|Gamma|Delta|Sigma|Omega|Neon|Radiance|Scourge|Templar|Uprising|Vigil|Wildfire|Exile|Ymir|Zodiac|Avalon|Blackout|Catalyst|Dominion|Enigma|Forsaken|Grimoire|Horizon|Illusion|Jade|Karma|Luminous|Mirage|Nebula|Oracle|Prestige|Quicksand|Rebirth|Sanctum|Tranquil|Unbroken|Virtue|Wanderer|Xcalibur|Yonder|Zodiac|Aegis|Bane|Cinder|Divine|Echo|Flux|Gale|Haven|Infinity|Justice|Kings|Loyalty|Majesty|Noble|Oath|Purity|Quest|Royal|Sacred|Triumph|Unity|Valor|Wisdom|Xenith|Yield|Zest|Aurora|Blaze|Crest|Dawn|Elite|Fable|Guardian|Honor|Ignite|Jinx|Knightly|Legend|Mythos|Nimbus|Outlaw|Prowler|Quasar|Rogue|Sable|Talon|Unseen|Vandal|Wicked|Xenon|Yonder|Zephyr|Aether|Bulwark|Cipher|Dusk|Elysium|Frost|Grim|Hollow|Icarus|Jester|Kaiser|Lynx|Mystic';
+$clubNames = explode('|', $clubNames);
+$clubNames = array_unique($clubNames);
+shuffle($clubNames);
 
-if (!$id) {
-    return;
-}
-
-$seasonInfo = Config::get('club/season', $id, false);
-if (!$init && (!$seasonInfo || $seasonInfo['seasonStart'] != date('Y-m-d'))) {
-    return;
-}
-
-$gradeConfig = Config::get('club/grade', $id - 1, false);
-if (!$gradeConfig) {
-    return;
-}
 //根据赛季俱乐部信息
 $commCfg = Config::get('club/common');
-
-$num = (int)array_sum($commCfg['scale']);
-if ($num <= 0) return;
-$aiClubs = [];
+$gradeConfig = Config::get('club/grade');
 $aiCfg = Config::get('club/ai');
 $levelCfg = Config::get('club/level');
-$cntLevel = array_column($levelCfg, 'clubLevel', 'member');
-foreach ($aiCfg as $aiInfo) {
-    if ($aiInfo['grade'] != $gradeConfig['grade']) {
-        continue;
-    }
-    $level = $aiInfo['level'];
-    if (!isset($commCfg['scale'][$level - 1])) {
-        continue;
-    }
-    $cunt = ceil($commCfg['aiClub'] * $commCfg['scale'][$level - 1] / $num);
-
-    for ($i = 0; $i < $cunt; $i++) {
-        $ukey = (int)($aiInfo['id'] . ($level + $i));
-        if (!Bll::club()->isAiClub($ukey)) {
+$defClubId = 9000;
+$aiClubs = [];
+foreach ($gradeConfig as $gradeInfo) {
+    $num = (int)array_sum($gradeInfo['aiProportion']);
+    $cntLevel = array_column($levelCfg, 'clubLevel', 'member');
+    foreach ($aiCfg as $aiInfo) {
+        if ($aiInfo['grade'] != $gradeInfo['grade']) {
             continue;
         }
-        $aiClubs[] = [
-            'clubId' => $ukey,
-            'creator' => $ukey,
-            'dan' => $gradeConfig['id'],
-            'type' => ClubBll::TYPE_PRIVATE,
-            'ai' => 1,
-            'level' => $cntLevel[$aiInfo['member']] ?? 1,
-            'memberCnt' => $aiInfo['member'],
-            'aiActLevel' => $level,
-            'clubName' => createNonceStr(15),
-        ];
+        $level = $aiInfo['level'];
+        if (!isset($gradeInfo['aiProportion'][$level - 1])) {
+            continue;
+        }
+        $cunt = ceil($commCfg['aiClub'] * $gradeInfo['aiProportion'][$level - 1] / $num);
+
+        for ($i = 0; $i < $cunt; $i++) {
+            $clubName = array_pop($clubNames);
+            if(!$clubName) {
+                break;
+            }
+            $aiClubs[] = [
+                'clubId' => $defClubId++,
+                'creator' => $defClubId,
+                'dan' => $gradeInfo['id'],
+                'type' => ClubBll::TYPE_PRIVATE,
+                'ai' => 1,
+                'level' => $cntLevel[$aiInfo['member']] ?? 1,
+                'memberCnt' => $aiInfo['member'],
+                'aiActLevel' => $level,
+                'clubName' => $clubName,
+            ];
+        }
     }
 }
 
 if ($aiClubs) {
     Model::clubs()->insertMulti($aiClubs);
 }
-
